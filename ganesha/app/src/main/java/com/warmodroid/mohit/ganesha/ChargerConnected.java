479f8fe881ed6091d88d8e283836d850893ea439
warmodroid <mohit.agrawal886@gmail.com>
2016-09-08 17:10:37 -0800

    Warmodroid

--- ganesha/app/src/main/java/com/warmodroid/mohit/ganesha/ChargerConnected.java
+++ ganesha/app/src/main/java/com/warmodroid/mohit/ganesha/ChargerConnected.java
@@ -1,20 +1,49 @@
 package com.warmodroid.mohit.ganesha;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.Toast;
 
 /**
  * Created by root on 2/8/16.
  */
 public class ChargerConnected extends BroadcastReceiver {
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.icon)
+                .setContentTitle("Charger Connected")
+                .setContentText("You will be notified when charging will be done");
+        Intent resultIntent = new Intent(context, MainActivity.class);
+        // The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
         Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         // Vibrate for 500 milliseconds
         v.vibrate(400);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,mBuilder.build());
         Toast.makeText(context, "Charger Connected", Toast.LENGTH_LONG).show();
         context.startService(new Intent(context,service.class));
     }
