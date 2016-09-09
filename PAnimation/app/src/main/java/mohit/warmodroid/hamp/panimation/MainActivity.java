af77131976f390f69e139b4d8ee3ca5d1582aaf8
warmodroid <mohit.agrawal886@gmail.com>
2016-09-09 11:01:36 -0800

    Android Animation

--- PAnimation/app/src/main/java/mohit/warmodroid/hamp/panimation/MainActivity.java
+++ PAnimation/app/src/main/java/mohit/warmodroid/hamp/panimation/MainActivity.java
@@ -16,18 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView textViewOne = (TextView) findViewById(R.id.textView1);
         final TextView textViewTwo = (TextView) findViewById(R.id.textView2);
+        final TextView textViewThree = (TextView) findViewById(R.id.textView3);
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.layout);
         final Animation a = AnimationUtils.loadAnimation(this,R.anim.textviewone);
         final Animation b = AnimationUtils.loadAnimation(this,R.anim.textviewtwo);
+        final Animation c = AnimationUtils.loadAnimation(this,R.anim.textviewthree);
         a.reset();
         b.reset();
+        c.reset();
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textViewOne.startAnimation(a);
                 textViewTwo.startAnimation(b);
+                textViewThree.startAnimation(c);
                 a.setDuration(10000);
                 b.setDuration(10000);
+                c.setDuration(10000);
             }
         });
     }
