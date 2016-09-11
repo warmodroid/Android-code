4f71c14902c25a8c666a3850d255d038ba37f873
warmodroid <mohit.agrawal886@gmail.com>
2016-09-11 12:15:43 -0800

    Custom View with Animation

--- CustomViewTwo/app/src/main/java/mohit/warmodroid/hamp/customviewtwo/MainActivity.java
+++ CustomViewTwo/app/src/main/java/mohit/warmodroid/hamp/customviewtwo/MainActivity.java
@@ -1,8 +1,11 @@
 package mohit.warmodroid.hamp.customviewtwo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ColorOptionsView myView = (ColorOptionsView) findViewById(R.id.circle);
-        myView.setCirlceText("Hey its working now...!!!");
+        final ColorOptionsView myView = (ColorOptionsView) findViewById(R.id.circle);
+        myView.setCirlceText("Click on me to see magic");
         myView.setCircleTextSize(20);
         myView.setCircleRaduis(150);
+        final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim_button);
+        animation.reset();
+        myView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You clicked this circle",Toast.LENGTH_LONG).show();
+                myView.setCircleColor(Color.GREEN);
+                myView.startAnimation(animation);
+                animation.setDuration(10000);
+            }
+        });
     }
 
 }
