f1ab95c7e03cb1d53ef29fb82a5458b13fb8f83c
warmodroid <mohit.agrawal886@gmail.com>
2016-09-12 11:43:20 -0800

    Battery Charging Animation

--- PBatteryChargingAnimation/app/src/main/java/mohit/warmodroid/hamp/pbatterycharginganimation/MainActivity.java
+++ PBatteryChargingAnimation/app/src/main/java/mohit/warmodroid/hamp/pbatterycharginganimation/MainActivity.java
@@ -1,5 +1,6 @@
 package mohit.warmodroid.hamp.pbatterycharginganimation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,22 +9,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView ;
+    AnimationDrawable ani;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        imageView = (ImageView) findViewById(R.id.imagee);
+        imageView.setBackgroundResource(R.drawable.gube);
+        ani = (AnimationDrawable) imageView.getBackground();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                ani.start();
             }
         });
     }
@@ -44,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
